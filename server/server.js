d71bf33287134cb80d9ff112e0f1a1022236b0ba
Liam Carey <lpc37@drexel.edu>
2019-08-28 10:19:59 -0400

    Quick updates to dotenv and cleaning up the serverdotjs file.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 //Requirements
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 
-/// REPLACE ///
 var mysql = require('mysql');
-var con = mysql.createCOnnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: '<DATABASE NAME>'
+var con = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_ENV
 });
-/// REPLACE ///
 
 con.connect(function(err) {
 	if (err) {
@@ -19,7 +18,9 @@ con.connect(function(err) {
 		console.log(err);
 	}
 	else {
-		console.log("")
+		console.log("Successfully connected to database.")
+	}
+	});
 
 app.use(express.static("."));
 app.listen(3000, function() {
@@ -27,26 +28,28 @@ app.listen(3000, function() {
 });
 
 app.post("/Auth", function(req, res) {
-	
+	////Auth
+	//token for header
+	//message in json
 	
 });
 
 app.get("/Notes", function(req,res) {
-	
+	////Notes
+	//all info from notes
+	//list of notes under key "data"
 	
 });
 
-/Auth
-token for header
-message in json
+app.get("/Save", function(req,res) {
+	//Please note this is just a temp placeholder, probably will be a post request to the api
+	//Also, let me know of a better name for this process
+	
+	
+	////Save
+	//sends update to db
+	//token in header
+	
+});
 
-/Notes
-all info from notes
-list of notes under key "data"
 
-/Save
-sends update to db
-token in header
-
-
-create .env

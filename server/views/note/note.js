b2afa405ad798d9f9008c8a458c4e384ee652579
Jon Corbin <jon.corbin8@gmail.com>
2019-08-28 22:26:54 -0400

    Add keyup for autosave

--- server/views/note/note.js
+++ server/views/note/note.js
@@ -20,7 +20,33 @@ function addAutoSave(){
       }, 750);
   });
 
+  $('#editArea').keyup(function () {
+    $('#status').attr('class', 'pending').text('Changes Pending');
+
+    // If a timer was already started, clear it.
+    if (timeout) clearTimeout(timeout);
+
+    // Set timer that will save comment when it fires.
+    timeout = setTimeout(function () {
+        saveNote();
+        $('#status').attr('class', 'saved').text('Changes Saved');
+      }, 750);
+  });
+
   $('#title').keypress(function () {
+    $('#status').attr('class', 'pending').text('Changes Pending');
+
+    // If a timer was already started, clear it.
+    if (timeout) clearTimeout(timeout);
+
+    // Set timer that will save comment when it fires.
+    timeout = setTimeout(function () {
+        saveNote();
+        $('#status').attr('class', 'saved').text('Changes Saved');
+      }, 750);
+  });
+
+  $('#title').keyup(function () {
     $('#status').attr('class', 'pending').text('Changes Pending');
 
     // If a timer was already started, clear it.

c1c3faadd6c8334cc6c24f518b57f1e8addf5a74
Liam Carey <lpc37@drexel.edu>
2019-08-28 20:48:46 -0400

    Updating serverdotjs file with query implementations.

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ con.connect(function(err) {
 	}
 	});
 
+//Function that generates a key for the user
+function genToken() {
+	//Generate a key using a method descibed later
+	return "zza67n&dhs09jj12#"
+}
+
+
+
 app.use(express.static("."));
 app.listen(3000, function() {
 	console.log("Server started.")
@@ -32,6 +40,57 @@ app.post("/Auth", function(req, res) {
 	//token for header
 	//message in json
 	
+	//Setting up the token response and query for authentication and token reserve
+	var authObj = { 'user' : req.query.userName, 'token' : genToken(), 'message' : '' };
+	var userCorrect = 0;
+	var authQuery = '';
+	//var authQuery = <AUTHQUERY> with req.query.userName
+	var tokenQuery = '';
+	//var tokenQuery = <TOKENQUERY> using authObj.token and authObj.userName
+	
+	//Querying for username
+	con.query(authQuery,
+		function(err,rows,fields) {
+		if (err) {
+			console.log('Error during processing.');
+			console.log(err);
+		}
+		else {
+			console.log("Obtained a response.")
+			if (rows.size == 0) {
+				authObj.message = 'Username was incorrect.';
+				res.write(authObj);
+				res.end();
+			}
+			else if (rows[0].userName != req.query.userName) {
+				authObj.message = 'Password was incorrect.';
+				res.write(authObj);
+				res.end();
+			}
+			else {
+				userCorrect = 1;
+			}
+		});
+	
+	//Redirecting and sending information back to the user
+	if (userConnect == 1) {
+		con.query(tokenQuery, 
+			function(err, result) {
+			if (err) {
+				console.log('Error during insertion.');
+				console.log(err);	
+			}
+			else {
+				console.log('Successfully inserted rows');
+				authObj.message = 'Login was successful.';
+				res.write(authObj);
+				res.redirect('http://localhost:8080/Home')
+				res.end();
+			}
+			});
+	}
+	
+	
 });
 
 app.get("/Notes", function(req,res) {
@@ -39,16 +98,68 @@ app.get("/Notes", function(req,res) {
 	//all info from notes
 	//list of notes under key "data"
 	
+	//Setting up token, username, and query variables
+	var tokenValue = req.headers.token;
+	var userNameValue = req.headers.userName;
+	var tokenVerified = 0;
+	var responseObj = { 'notes' : {}, 'message' : '' };
+	var tokenQuery = '';
+	//var tokenQuery = <TOKENQUERY> with userNameValue and tokenValue
+	var notesQuery = '';
+	//var noteQuery = <NOTESQUERY> using userNameValue
+
+	//Querying for token
+	con.query(tokenQuery,
+		function(err,rows,fields) {
+		if (err) {
+			console.log('Error during processing.');
+			console.log(err);
+		}
+		else {
+			if (rows.size == 0) {
+				responseObj.message = 'No token was found for this username.';
+				res.write(responseObj);
+				res.end();	
+			}
+			else if (rows[0].token != tokenValue) {
+				responseObj.message = 'Token is incorrect.';
+				res.write(responseObj);
+				res.end();
+			}
+			else {
+				tokenVerified = 1;
+			}
+		}
+		});
+	
+	//Querying for the notes from the database
+	if (tokenVerified == 1) {
+		con.query(noteQuery,
+			function(err,rows,fields){
+			if (err) {
+				console.log('Error during processing.');
+				console.log(err);
+			}
+			else {
+				console.log('Obtained a response.');
+				repsonseObj.notes = rows;
+				responseObj.message = 'Successfully gained a response.';
+				res.write(responseObj);
+				res.end();
+			}
+			});
+	}
 });
 
 app.get("/Save", function(req,res) {
 	//Please note this is just a temp placeholder, probably will be a post request to the api
 	//Also, let me know of a better name for this process
 	
-	
 	////Save
 	//sends update to db
 	//token in header
+	
+	
 	
 });
 

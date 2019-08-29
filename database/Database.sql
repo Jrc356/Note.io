710ee7a7794759979c5e9f47137a8de0daa60f93
Jon Corbin <jon.corbin8@gmail.com>
2019-08-29 19:15:11 -0400

    Remove insert into auto-inc fields; remove userID foreign key in token table

--- database/Database.sql
+++ database/Database.sql
@@ -15,17 +15,17 @@ CREATE TABLE userTable(
 	PRIMARY KEY (userID)
 );
 
-INSERT INTO userTable(userID, userName, userEmail, userPassword)
-VALUES ("000001", "user1","user1@notes.io","default1");
+INSERT INTO userTable(userName, userEmail, userPassword)
+VALUES ("user1","user1@notes.io","default1");
+
+INSERT INTO userTable(userName, userEmail, userPassword)
+VALUES ("user2","user2@notes.io","default2");
 
 INSERT INTO userTable(userID, userName, userEmail, userPassword)
-VALUES ("000002", "user2","user2@notes.io","default2");
+VALUES ("user3","user3@notes.io","default3");
 
 INSERT INTO userTable(userID, userName, userEmail, userPassword)
-VALUES ("000003", "user3","user3@notes.io","default3");
-
-INSERT INTO userTable(userID, userName, userEmail, userPassword)
-VALUES ("000004", "user4","user4@notes.io","default4");
+VALUES ("user4","user4@notes.io","default4");
 
 
 CREATE TABLE userNotes(
@@ -39,39 +39,36 @@ CREATE TABLE userNotes(
 	CONSTRAINT FK_Owner FOREIGN KEY(OwnerID) REFERENCES userTable(userID)
 );
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000001", "To Do List", "Homework: Math, English", "000001");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("To Do List", "Homework: Math, English", "000001");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000002", "Example 2", "Example 2", "000001");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 2", "Example 2", "000001");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000003", "Example 3", "Example 3", "000002");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 3", "Example 3", "000002");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000004", "Example 4", "Example 4", "000002");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 4", "Example 4", "000002");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000005", "Example 5", "Example 5", "000003");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 5", "Example 5", "000003");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000006", "Example 6", "Example 6", "000003");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 6", "Example 6", "000003");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000007", "Example 7", "Example 7", "000004");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 7", "Example 7", "000004");
 
-INSERT INTO userNotes(notesID, Title, Content, ownerID)
-VALUES ("000008", "Example 8", "Example 8", "000004");
+INSERT INTO userNotes(Title, Content, ownerID)
+VALUES ("Example 8", "Example 8", "000004");
 
 
 CREATE TABLE userToken(
 	tokenID int NOT NULL AUTO_INCREMENT,
-	userID int,
 	tokenStr varchar(225),
 	
 	PRIMARY KEY (tokenID),
-	CONSTRAINT FK_userID FOREIGN KEY(userID) REFERENCES userTable(userID)
-
 )
 
 

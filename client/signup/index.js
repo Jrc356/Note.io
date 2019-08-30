9d381b53c87eec3603d23a964596e56bc4e729c8
Jon Corbin <jon.corbin8@gmail.com>
2019-08-29 22:11:30 -0400

    Fix signup functionality

--- client/signup/index.js
+++ client/signup/index.js
@@ -1,21 +1,29 @@
 function signup(){
-	var username = document.getElementById("User").value //get username input from textbox
-	var password = document.getElementById("Password").value //get password input from textbox
-	
-	$.ajax({
-		type: "POST",
-		url: 'http://localhost:3000/addUser',
-		data: {
-			username,
-			password
-		},
-		success: function(data) {
-			sessionStorage.setItem(data.token);
-			window.location = '/home';
-		},    
-		error: function(err) {
-			document.getElementById("maindiv").innerHTML = "Invalid Combination of Username and Password";
-			console.log(err);
-		}
-	});	
+	const username = document.getElementById("User").value //get username input from textbox
+	const password = document.getElementById("Password").value //get password input from textbox
+	const confirmPass = $('#ConfirmPassword').val();
+
+	console.log(password);
+	console.log(confirmPass);
+	if (password !== confirmPass){
+		$('#maindiv').html('Your passwords do not match');
+	} else {
+		$.ajax({
+			type: "POST",
+			url: 'http://localhost:3000/addUser',
+			data: {
+				username,
+				password
+			},
+			success: function(data) {
+				console.log('signed up successfully');
+				sessionStorage.setItem('token', data.token);
+				window.location = '/home';
+			},    
+			error: function(err) {
+				document.getElementById("maindiv").innerHTML = "That username is already taken";
+				console.log(err);
+			}
+		});
+	}	
 }

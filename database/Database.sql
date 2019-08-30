a8329e5fe4f700ca72324854c05a910581ffd780
Jon Corbin <jon.corbin8@gmail.com>
2019-08-29 20:02:00 -0400

    replace userID with userName; make userName primary key for user

--- database/Database.sql
+++ database/Database.sql
@@ -7,11 +7,10 @@ GRANT ALL PRIVILEGES ON Notes.* TO 'noteio'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE TABLE userTable(
-	userID int NOT NULL AUTO_INCREMENT,
-	userName varchar(225),
+	userName varchar(225) NOT NULL,
 	userPassword varchar(225),
 	
-	PRIMARY KEY (userID)
+	PRIMARY KEY (userName)
 );
 
 INSERT INTO userTable(userName, userPassword)
@@ -31,45 +30,45 @@ CREATE TABLE userNotes(
 	notesID int NOT NULL AUTO_INCREMENT,
 	Title varchar(225),
 	Content TEXT,
-	ownerID int,
+	userName varchar(225),
 	
 	PRIMARY KEY (notesID), 
 	
-	CONSTRAINT FK_Owner FOREIGN KEY(OwnerID) REFERENCES userTable(userID)
+	CONSTRAINT FK_Owner FOREIGN KEY(userName) REFERENCES userTable(userName)
 );
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("To Do List", "Homework: Math, English", "000001");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("To Do List", "Homework: Math, English", "user1");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 2", "Example 2", "000001");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 2", "Example 2", "user1");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 3", "Example 3", "000002");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 3", "Example 3", "user2");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 4", "Example 4", "000002");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 4", "Example 4", "user2");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 5", "Example 5", "000003");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 5", "Example 5", "user3");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 6", "Example 6", "000003");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 6", "Example 6", "user3");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 7", "Example 7", "000004");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 7", "Example 7", "user4");
 
-INSERT INTO userNotes(Title, Content, ownerID)
-VALUES ("Example 8", "Example 8", "000004");
+INSERT INTO userNotes(Title, Content, userName)
+VALUES ("Example 8", "Example 8", "user4");
 
 
 CREATE TABLE userToken(
 	tokenID int NOT NULL AUTO_INCREMENT,
-	userID int,
+	userName varchar(225),
 	tokenStr varchar(225),
 	
 	PRIMARY KEY (tokenID),
-	CONSTRAINT FK_userID FOREIGN KEY(userID) REFERENCES userTable(userID)
+	CONSTRAINT FK_userID FOREIGN KEY(userName) REFERENCES userTable(userName)
 );
 
 
